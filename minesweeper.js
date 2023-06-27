f03f3420ccdb7ce66cd40af05cf001e632304079
Makádi Balázs <makadi.balazs@gmail.com>
2023-06-27 13:47:16 +0200

    Tábla kirajzolása

--- minesweeper.js
+++ minesweeper.js
@@ -3,9 +3,19 @@ const canvas = document.getElementById('myCanvas');
 const c = canvas.getContext('2d');
 
 const size = 50;
-let x = 0;
-let y = 0;
+const columns = canvas.width / size;
+const rows = canvas.height / size;
+
+function drawMap() {
+  for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < columns; i++) {
+      drawImage(i * size, j * size);
+    }
+  }
+}
 
 function drawImage(x, y) {
   c.drawImage(image, x, y, size, size);
 }
+
+drawMap();
