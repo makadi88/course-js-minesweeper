80902cf83b07c59ed82bc670604d7a13ffd00341
Makádi Balázs <makadi.balazs@gmail.com>
2023-06-27 13:14:51 +0200

    drawImage függvény létrehozása

--- minesweeper.js
+++ minesweeper.js
@@ -1,1 +1,11 @@
-console.log('hello világ');
+const image = document.getElementById('hidden');
+const canvas = document.getElementById('myCanvas');
+const c = canvas.getContext('2d');
+
+const size = 50;
+let x = 0;
+let y = 0;
+
+function drawImage(x, y) {
+  c.drawImage(image, x, y, size, size);
+}
