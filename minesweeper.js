5e135b4863b70553fcebad3c1f979dece429206f
Makádi Balázs <makadi.balazs@gmail.com>
2023-07-02 13:23:52 +0200

    Random aknak lerakása függvény

--- minesweeper.js
+++ minesweeper.js
@@ -5,6 +5,22 @@ const c = canvas.getContext('2d');
 const size = 50;
 const columns = canvas.width / size;
 const rows = canvas.height / size;
+const mine = 'mine';
+const minecount = 5;
+
+let map = createMap();
+
+function placeMines(map, minecount) {
+  let mines = 0;
+  while (mines < minecount) {
+    let x = Math.floor(Math.random() * columns);
+    let y = Math.floor(Math.random() * rows);
+    if (map[y][x] !== mine) {
+      map[y][x] = mine;
+      mines++;
+    }
+  }
+}
 
 function drawMap() {
   for (let j = 0; j < rows; j++) {
@@ -18,4 +34,17 @@ function drawImage(x, y) {
   c.drawImage(image, x, y, size, size);
 }
 
+function createMap() {
+  let map = [];
+  for (let j = 0; j < rows; j++) {
+    let row = [];
+    for (let i = 0; i < columns; i++) {
+      row[i] = 0;
+    }
+    map[j] = row;
+  }
+  return map;
+}
+
 drawMap();
+console.log(map);
