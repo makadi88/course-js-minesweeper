c524c12049605be5fe6176fc6f22e5effcc4f350
Makádi Balázs <makadi.balazs@gmail.com>
2023-07-09 20:07:58 +0200

    Valami lett itt

--- minesweeper.js
+++ minesweeper.js
@@ -5,7 +5,7 @@ const size = 50;
 const columns = canvas.width / size;
 const rows = canvas.height / size;
 const mine = 'mine';
-const minecount = 5;
+const mineCount = 20;
 const images = {
   hidden: document.getElementById('hidden'),
   mine: document.getElementById('mine'),
@@ -21,11 +21,52 @@ const images = {
 };
 
 let map = createMap();
-placeMines(map, minecount);
+placeMines(map, mineCount);
+calculateFieldValues(map);
+drawMap();
 
-function placeMines(map, minecount) {
+function calculateFieldValues(map) {
+  for (let rowI = 0; rowI < rows; rowI++) {
+    for (let colI = 0; colI < columns; colI++) {
+      let field = map[rowI][colI];
+      if (field !== mine) {
+        let neighbourCoordinates = findNeighbourFields(map, rowI, colI); // [{row: 7, col: 1}, {row: 7, col: 2}, ...]
+        let mineCount = countMines(map, neighbourCoordinates);
+        map[rowI][colI] = mineCount;
+      }
+    }
+  }
+}
+
+function countMines(map, coordinates) {
+  let mineCount = 0;
+  for (let i = 0; i < coordinates.length; i++) {
+    let coordinate = coordinates[i]; // {row: 7, col: 1}
+    let field = map[coordinate.row][coordinate.col];
+    if (field === mine) {
+      mineCount++;
+    }
+  }
+  return mineCount;
+}
+
+function findNeighbourFields(map, rowI, colI) {
+  let neighbourCoordinates = [];
+  for (let row = rowI - 1; row <= rowI + 1; row++) {
+    for (let col = colI - 1; col <= colI + 1; col++) {
+      if (row >= 0 && row < rows && col >= 0 && col < columns) {
+        if (row !== rowI || col !== colI) {
+          neighbourCoordinates.push({ row: row, col: col });
+        }
+      }
+    }
+  }
+  return neighbourCoordinates;
+}
+
+function placeMines(map, mineCount) {
   let mines = 0;
-  while (mines < minecount) {
+  while (mines < mineCount) {
     let x = Math.floor(Math.random() * columns);
     let y = Math.floor(Math.random() * rows);
     if (map[y][x] !== mine) {
@@ -33,6 +74,18 @@ function placeMines(map, minecount) {
       mines++;
     }
   }
+}
+
+function createMap() {
+  let map = [];
+  for (let j = 0; j < rows; j++) {
+    let row = [];
+    for (let i = 0; i < columns; i++) {
+      row[i] = 0;
+    }
+    map[j] = row;
+  }
+  return map;
 }
 
 function drawMap() {
@@ -48,17 +101,3 @@ function drawMap() {
 function drawImage(image, x, y) {
   c.drawImage(image, x, y, size, size);
 }
-
-function createMap() {
-  let map = [];
-  for (let j = 0; j < rows; j++) {
-    let row = [];
-    for (let i = 0; i < columns; i++) {
-      row[i] = 0;
-    }
-    map[j] = row;
-  }
-  return map;
-}
-
-drawMap();
